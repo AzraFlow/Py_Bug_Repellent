a312614ce9f2faeb6e89c55ac00c3a80c4629521
mbuhidar <mbuhidar@gmail.com>
2021-02-20 09:40:07 -0500

    Add comments to unit tests and improve output format.

--- tests/pybug_test.py
+++ tests/pybug_test.py
@@ -1,9 +1,16 @@
 from pybugrepellent.pybug import charlist_to_code_list
 from pybugrepellent.pybug import int_list_to_hilo_bytes
 from pybugrepellent.pybug import hex_to_repellent_code
+from pybugrepellent.pybug import repellent_code_to_hex
 
 
 def test_ctcl_short_string():
+    '''
+    Unit test to check the charlist_to_code_list() function
+    for proper conversion of a line of code to a list of
+    corresponding c64 petscii codes as integers.  Focus on
+    short lines of code.
+    '''
     short_list = "A"
     short_list_char_list = charlist_to_code_list(short_list)
     assert short_list_char_list == [65]
@@ -13,6 +20,12 @@ def test_ctcl_short_string():
 
 
 def test_ctcl_alphanumonly():
+    '''
+    Unit test to check the charlist_to_code_list() function
+    for proper conversion of a line of code to a list of
+    corresponding c64 petscii codes as integers.  Focus on
+    lines of code with all alpha numeric characters.
+    '''
     alpha_num = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^"
     alpha_num_char_list = charlist_to_code_list(alpha_num)
     assert alpha_num_char_list == [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
@@ -25,8 +38,11 @@ def test_ctcl_alphanumonly():
 
 def test_ctcl_ahoy_special_char_inline():
     '''
-    Unit test to check for detection and handling of special characters
-    represented within braces embedded inline with other characters.
+    Unit test to check the charlist_to_code_list() function
+    for proper conversion of a line of code to a list of
+    corresponding c64 petscii codes as integers.  Focus on 
+    detection and handling of special characters represented
+    within braces embedded inline with other characters.
     '''
     inline_spec_char = "AB{WH}CD"
     inline_spec_char_list = charlist_to_code_list(inline_spec_char)
@@ -35,8 +51,11 @@ def test_ctcl_ahoy_special_char_inline():
 
 def test_ctcl_ahoy_special_chars():
     '''
-    Unit test to check for detection and accuracy of all Ahoy special characters
-    represented within braces.
+    Unit test to check the charlist_to_code_list() function
+    for proper conversion of a line of code to a list of
+    corresponding c64 petscii codes as integers.  Focus on
+    check for detection and accuracy of all Ahoy special
+    characters represented within braces.
     '''
     all_spec_char = "{WH}{CD}{RV}{HM}{RD}{CR}{GN}{BL}{OR}{F1}{F2}{F3}{F4}{F5}{F6}{F7}{F8}{BK}{CU}{RO}{SC}{IN}{BR}{LR}{G1}{G2}{LG}{LB}{G3}{PU}{CL}{YL}{CY}{SS}"
     all_spec_char_list = charlist_to_code_list(all_spec_char)
@@ -48,9 +67,12 @@ def test_ctcl_ahoy_special_chars():
 
 def test_ctcl_detect_leading_closing_brace():
     '''
-    Unit test to check for detection and error reporting of leading closing
-    brace character "}" - a condition that should never happen in properly 
-    typed in code.
+    Unit test to check the charlist_to_code_list() function
+    for proper conversion of a line of code to a list of
+    corresponding c64 petscii codes as integers.  Focus on
+    check for detection and error reporting of leading
+    closing brace character "}" - a condition that should
+    never happen in properly typed in code.
     '''
     leading_close_brace = "AB}{WH}"
     code_list_close_brace = charlist_to_code_list(leading_close_brace)
@@ -81,6 +103,17 @@ def test_hex_to_repellent_code():
     alpha_code = hex_to_repellent_code(hex_value)
     assert alpha_code == "AB"
 
+
+def test_repellent_code_to_hex():
+    '''
+    Unit test to check that function repellent_code_to_hex()
+    returns the passed in alpha code combination as a hex value
+    '''
+    alpha_code = "BN"
+    hex_value = repellent_code_to_hex(alpha_code)
+    assert hex_value == "0x1d"
+
+'''
 def test_prog_flow():
     codeline = "GGGG"
     hilo_bytes = int_list_to_hilo_bytes(charlist_to_code_list(codeline))
@@ -88,3 +121,4 @@ def test_prog_flow():
     hex_xor_hilo = hex(xor_hilo)
     alpha_code = hex_to_repellent_code(hex_xor_hilo)
     assert alpha_code == "CB"
+'''
